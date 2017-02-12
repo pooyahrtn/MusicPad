90e96bc056326b80927c83058f37c99ab866104a
pooya <pooya.haratian@gmail.com>
2017-02-12 21:59:05 +0330

    first commit maybe

--- app/src/main/java/ir/mp/musicpad/MainActivity.java
+++ app/src/main/java/ir/mp/musicpad/MainActivity.java
@@ -8,60 +8,59 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.SparseArray;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MAIN_ACTIVITY";
     private DownloadManager downloadManager;
 
+    private static String saved_sound_name = "cartoon001.wav";
 
-    private List<SoundPool> mSoundPools;
+
+//    private SparseArray<SoundPool> mSoundPools;
+    private SparseArray<SoundButton> mButtonSparseArray;
+//    private int[] mSoundIDs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initSoundButtons();
+    }
 
-        mSoundPools = new ArrayList<>();
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mButtonSparseArray == null){
+            initSoundButtons();
+        }
         for (int i = 0; i < 12; i++) {
-            SoundPool soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
-            mSoundPools.add(soundPool);
-        }
-
-        if (isExternalStorageReadable()&& isExternalStorageWritable()){
-            Log.e(TAG, "onCreate: accessable" );
-            long id = downloadData(Uri.parse(downloadTestAddress));
-            Log.e(TAG, "onCreate: id = " + String.valueOf(id) );
+           SoundButton soundButton = mButtonSparseArray.get(i);
+            if (soundButton.getSoundPool() == null){
+                SoundPool soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+                int mSoundID = soundPool
+                        .load(getFile(saved_sound_name).getPath(), 1);
+                soundButton.setSoundId(mSoundID);
+                soundButton.setSoundPool(soundPool);
+            }
         }
 
 
     }
 
+    //    static String downloadTestAddress = "http://dl.nex1music.ir/1395/11/17/Ali%20Sotoodeh%20-%20Hesse%20Tanhaei%20[128].mp3";
 
 
-    static String downloadTestAddress = "http://dl.nex1music.ir/1395/11/17/Ali%20Sotoodeh%20-%20Hesse%20Tanhaei%20[128].mp3";
 
-
-    public void save_sound(){
-        DownloadManager.Request req = new DownloadManager.Request(Uri.parse("http://dl.nex1music.ir/1395/11/17/Ali%20Sotoodeh%20-%20Hesse%20Tanhaei%20[128].mp3"));
-        req.setTitle("file tit").setDescription("Downloading ....") // download the package at the /sdcard/download path.
-                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "k_one"+ ".mp3");
-
-    }
-
-    public File getMusicStorageDir(String albumName) {
-        // Get the directory for the user's public pictures directory.
-        File file = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), albumName);
-        if (!file.mkdirs()) {
-            Log.e(TAG, "Directory not created");
-        }
-        return file;
-    }
 
     public boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
@@ -97,13 +96,36 @@ public class MainActivity extends AppCompatActivity {
 
         //Set the local destination for the downloaded file to a path within the application's external files directory
 
-        request.setDestinationInExternalFilesDir(MainActivity.this, Environment.DIRECTORY_DOWNLOADS,"AndroidTutorialPoint.mp3");
+        request.setDestinationInExternalFilesDir(MainActivity.this, Environment.DIRECTORY_DOWNLOADS,saved_sound_name);
 
         //Enqueue download and save into referenceId
         downloadReference = downloadManager.enqueue(request);
 
 
         return downloadReference;
+    }
+    public File getFile(String dbFilePath) {
+
+        // Create full path
+        String picturePath = Environment.DIRECTORY_DOWNLOADS.concat(File.separator).concat(
+                dbFilePath);
+        Log.e(TAG, "getFile: file_path " + picturePath );
+        // Create file
+        File mFile = getExternalFilesDir(picturePath);
+
+        return mFile;
+    }
+
+    private void initSoundButtons(){
+        if (mButtonSparseArray == null){
+            mButtonSparseArray = new SparseArray<>(12);
+        }
+        for (int i = 0; i < 12; i++) {
+            int buttonId = getResources().getIdentifier("sound_button_"+ String.valueOf(i+1), "id", getApplicationContext().getPackageName());
+            // TODO: 2/11/17 : add buttons manually to onCreate(). its not good for performance.
+            SoundButton soundButton = (SoundButton)findViewById(buttonId);
+            mButtonSparseArray.put(i ,soundButton);
+        }
     }
 
 }
--- app/src/main/java/ir/mp/musicpad/SoundButton.java
+++ app/src/main/java/ir/mp/musicpad/SoundButton.java
@@ -3,21 +3,30 @@ package ir.mp.musicpad;
 import android.content.Context;
 import android.media.SoundPool;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by apple on 2/10/2017 AD.
  */
 
-public class SoundButton extends View {
+public class SoundButton extends ImageButton {
+    public static final String TAG = "SOUND_BUTTON";
 
     private SoundPool mSoundPool;
+    private int mSoundId;
 
     public SoundButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initButton();
     }
     public SoundButton(Context context ){
         super(context);
+        initButton();
     }
 
     public void setSoundPool(SoundPool soundPool) {
@@ -26,6 +35,46 @@ public class SoundButton extends View {
 
     public SoundPool getSoundPool() {
         return mSoundPool;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            Log.e(TAG, "onTouchEvent: "+String.valueOf(mSoundId));
+            mSoundPool.play(mSoundId , 1, 1, 0,0,1);
+
+            //// TODO: 2/11/17 :  it should be a better way to do that. search for performance.
+            setBackgroundResource(R.drawable.sound_button_pressed);
+            return true;
+        }
+        else if (event.getAction() == MotionEvent.ACTION_UP){
+            setBackgroundResource(R.drawable.sound_button_default);
+            return true;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        ViewGroup.LayoutParams layoutParams = getLayoutParams();
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int dimension = Math.min(width , height);
+        setMeasuredDimension(dimension, dimension);
+    }
+
+    public void setSoundId(int soundId) {
+        mSoundId = soundId;
+    }
+
+    public int getSoundId() {
+        return mSoundId;
+    }
+
+    private void initButton(){
+        setBackgroundResource(R.drawable.sound_button_default);
     }
 }
 
